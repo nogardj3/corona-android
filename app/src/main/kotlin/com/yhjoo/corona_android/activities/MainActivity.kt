f0ae3f64579f1d3f3119713738c9a790b9619562
nogardj3 <nogardj3@github.com>
2021-08-10 00:18:27 +0900

    update

--- app/src/main/kotlin/com/yhjoo/corona_android/activities/MainActivity.kt
+++ app/src/main/kotlin/com/yhjoo/corona_android/activities/MainActivity.kt
@@ -1,35 +1,37 @@
-package com.yhjoo.corona_android
+package com.yhjoo.corona_android.activities
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 
-class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+import com.yhjoo.corona_android.databinding.AMainBinding
+import com.yhjoo.corona_android.utils.Utils
 
+
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
     private lateinit var mMap: GoogleMap
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.a_main)
+
+        val binding = AMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-//        val mapFragment = supportFragmentManager
-//                .findFragmentById(R.id.map) as SupportMapFragment
-//        mapFragment.getMapAsync(this)
+        val mapFragment = supportFragmentManager
+            .findFragmentById(binding.map.id) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+
+        Utils.log("=======================")
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
