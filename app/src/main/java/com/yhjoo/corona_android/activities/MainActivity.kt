4e4a9e8e40945fa7f366ad5a228e93c7a765aea5
yhjoo <wwwfy@naver.com>
2021-09-15 09:13:01 +0900

    update

---
+++ app/src/main/java/com/yhjoo/corona_android/activities/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.yhjoo.corona_android.activities
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+
+import com.yhjoo.corona_android.databinding.AMainBinding
+import com.yhjoo.corona_android.utils.Utils
+
+
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+    private lateinit var mMap: GoogleMap
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val binding = AMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        val mapFragment = supportFragmentManager
+            .findFragmentById(binding.map.id) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+
+        Utils.log("=======================")
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        // Add a marker in Sydney and move the camera
+        val sydney = LatLng(-34.0, 151.0)
+        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    }
+}
